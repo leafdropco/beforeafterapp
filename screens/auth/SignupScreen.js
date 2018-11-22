1ca4f53c41809b73e6b54380a44c768a584e6a9f
Chris Daniel <christopherdaniel@ChristophersMBP.home>
2018-11-22 03:24:16 -0500

    Added FireStore to signup

--- screens/auth/SignupScreen.js
+++ screens/auth/SignupScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 import { LinearGradient } from 'expo';
 
@@ -28,9 +29,12 @@ export default class SignupScreen extends React.Component {
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(
         () => {
-          const ref = firebase.database().ref();
-          var usersRef = ref.child('users');
-          var userRef = usersRef.push({
+          const ref = firebase.firestore();
+          ref.settings({
+            timestampsInSnapshots: true,
+          })
+          var usersRef = ref.collection('users');
+          usersRef.add({
             firstName: this.state.firstName,
             lastName: this.state.lastName,
             yomiName: `${this.state.firstName} ${this.state.lastName}`,
@@ -49,26 +53,26 @@ export default class SignupScreen extends React.Component {
     return (
       <View style={{ height: `100%`, display: 'flex', flexDirection: 'column', backgroundColor: 'white' }}>
         <View style={{ height: `30%` }}>
-        <LinearGradient
-          colors={['#05809D', '#0AC9D9']}
-          style={{ width: `100%`, height: `100%`, alignItems: 'center' }}
-          start={{ x: 0, y: 1 }}
-          end={{ x: 1, y: 1 }}
-        >
-        <Text style={{ color: 'white', paddingTop: `25%`, fontSize: 24, marginBottom: 24 }}><Text>before /</Text><Text style={{ fontWeight: 'bold' }}>/ After</Text></Text>
-          <View style={{display: 'flex', flexGrow: 1}}></View>
-          <View style={{ display: 'flex', flexDirection: 'row', width: '100%'}}>
-            <View style={{textAlign: 'center'}}>
-              <Text style={{padding: 20, color: '#fff'}} onPress={()=> this.props.navigation.navigate("Login")}>Sign In</Text>
+          <LinearGradient
+            colors={['#05809D', '#0AC9D9']}
+            style={{ width: `100%`, height: `100%`, alignItems: 'center' }}
+            start={{ x: 0, y: 1 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Text style={{ color: 'white', paddingTop: `25%`, fontSize: 24, marginBottom: 24 }}><Text>before /</Text><Text style={{ fontWeight: 'bold' }}>/ After</Text></Text>
+            <View style={{ display: 'flex', flexGrow: 1 }}></View>
+            <View style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+              <View style={{ textAlign: 'center' }}>
+                <Text style={{ padding: 20, color: '#fff' }} onPress={() => this.props.navigation.navigate("Login")}>Sign In</Text>
+              </View>
+              <View style={{ padding: 20, textAlign: 'center', borderBottomColor: '#fff', borderBottomWidth: 2 }}>
+                <Text style={{ color: '#fff', fontWeight: 'bold' }}>New Account</Text>
+              </View>
             </View>
-            <View style={{padding: 20, textAlign: 'center', borderBottomColor: '#fff', borderBottomWidth: 2}}>
-              <Text style={{color: '#fff', fontWeight: 'bold'}}>New Account</Text>
-            </View>
-          </View>
           </LinearGradient>
-          </View>
-          <View style={{ padding: 20, paddingTop: 40, flex:1, alignItems:'center' }}>
-            <TextInput
+        </View>
+        <View style={{ padding: 20, paddingTop: 40, flex: 1, alignItems: 'center' }}>
+          <TextInput
             style={styles.input}
             value={this.state.email}
             onChangeText={text => {
@@ -78,20 +82,20 @@ export default class SignupScreen extends React.Component {
             keyboardType="email-address"
             autoCapitalize="none"
             autoCorrect={false}
-            />
+          />
 
-            <TextInput style={styles.input}
-              value={this.state.password}
-              onChangeText={text => {
-                this.setState({ password: text });
-              }}
-              placeholder="Password"
-              secureTextEntry={true}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
+          <TextInput style={styles.input}
+            value={this.state.password}
+            onChangeText={text => {
+              this.setState({ password: text });
+            }}
+            placeholder="Password"
+            secureTextEntry={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
 
-            <TextInput
+          <TextInput
             style={styles.input}
             value={this.state.passwordConfirm}
             onChangeText={text => {
@@ -101,9 +105,9 @@ export default class SignupScreen extends React.Component {
             secureTextEntry={true}
             autoCapitalize="none"
             autoCorrect={false}
-            />
+          />
 
-            <TextInput style={styles.input}
+          <TextInput style={styles.input}
             value={this.state.firstName}
             onChangeText={text => {
               this.setState({ firstName: text });
@@ -111,9 +115,9 @@ export default class SignupScreen extends React.Component {
             placeholder="First Name"
             autoCapitalize="words"
             autoCorrect={true}
-            />
+          />
 
-            <TextInput style={styles.input}
+          <TextInput style={styles.input}
             value={this.state.lastName}
             onChangeText={text => {
               this.setState({ lastName: text });
@@ -121,16 +125,16 @@ export default class SignupScreen extends React.Component {
             placeholder="Last Name"
             autoCapitalize="words"
             autoCorrect={true}
-            />
+          />
 
 
 
           <View style={styles.button} onPress={this.onSignupPress}>
-            <Text style={{color: '#0AC9D9', textAlign: 'center'}} onPress={this.onSignupPress}>Sign Up</Text>
+            <Text style={{ color: '#0AC9D9', textAlign: 'center' }} onPress={this.onSignupPress}>Sign Up</Text>
           </View>
 
-          </View>
-          </View>
+        </View>
+      </View>
     );
   }
 }
